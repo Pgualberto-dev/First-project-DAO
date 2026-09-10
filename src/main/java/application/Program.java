72cfe2bb96f6f7dea4d50f89faa70248b4841844
Pablo Gualberto <pablogozs19@gmail.com>
2026-09-10 15:34:38 -0300

    Implementa a conexão no SellerDaoJDBC e adiciona o método findById para buscar vendedores

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,11 +10,9 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", new Date(), 3000.0, department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
