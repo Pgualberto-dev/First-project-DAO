7ae34940fa3387b943d51980cf54c9e2973d99e6
Pablo Gualberto <pablogozs19@gmail.com>
2026-09-11 11:15:08 -0300

    Adiciona o método findByDepartment na interface SellerDao e implementa na classe SellerDaoJDBC; atualiza o Program para testar a nova funcionalidade

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,7 +4,7 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
-import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -14,6 +14,12 @@ public class Program {
 
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
-
+        System.out.println(seller);
+        System.out.println("=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
