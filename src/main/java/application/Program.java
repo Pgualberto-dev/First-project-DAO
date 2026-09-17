5e14fe412067a3df464939690438f88276d00665
Pablo Gualberto <pablogozs19@gmail.com>
2026-09-17 11:39:27 -0300

    Adiciona a classe DepartmentDaoJDBC com métodos para inserir, atualizar, deletar e buscar departamentos; atualiza o Program para testar as novas funcionalidades

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -13,7 +14,6 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
@@ -42,6 +42,36 @@ public class Program {
         int id = sc.nextInt();
         sellerDao.deleteById(id);
         System.out.println("Delete completed");
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: findById =======");
+        Department dep = departmentDao.findById(1);
+        System.out.println(dep);
+
+        System.out.println("\n=== TEST 2: findAll =======");
+        List<Department> list2 = departmentDao.findAll();
+        for (Department d : list2) {
+            System.out.println(d);
+        }
+
+        System.out.println("\n=== TEST 3: insert =======");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id: " + newDepartment.getId());
+
+        System.out.println("\n=== TEST 4: update =======");
+        Department dep2 = departmentDao.findById(1);
+        dep2.setName("Food");
+        departmentDao.update(dep2);
+        System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 5: delete =======");
+        System.out.print("Enter id for delete test: ");
+        int id1 = sc.nextInt();
+        departmentDao.deleteById(id1);
+        System.out.println("Delete completed");
+
         sc.close();
     }
 }
